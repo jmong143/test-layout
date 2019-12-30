89edb2e9df63749e0c6c80efe108a116adb7f596
jong <jong@unionbankph.com>
2019-12-30 17:29:56 +0800

    test

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const favicon = require('express-favicon');
 const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+//"start": "cross-env HTTPS=true react-scripts start", -- notes
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', function(req, res) {
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(9000);
+app.listen(port);
