623d74176209e8e3d5a53aa5430f462dac431b69
jong <jong@unionbankph.com>
2019-12-31 02:50:54 +0800

    Updates on gallery page

--- src/Gallery/index.js
+++ src/Gallery/index.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import ImageGallery from 'react-image-gallery';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faImage } from '@fortawesome/free-solid-svg-icons';
 import './styles.css';
 import './image-gallery.css';
 
 export default class GalleryComponent extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            infinite: true,
+            showBullets: true,
+            thumbnailPosition: 'right'
+        }
+     
+    }
     componentDidMount(){
         
     }
@@ -30,8 +41,7 @@ export default class GalleryComponent extends Component {
             {
                 original: process.env.PUBLIC_URL + '/images/5.jpg',
                 thumbnail: process.env.PUBLIC_URL + '/images/5_thumb.png'
-            }
-            ,
+            },
             {
                 original: process.env.PUBLIC_URL + '/images/6.jpg',
                 thumbnail: process.env.PUBLIC_URL + '/images/6_thumb.png'
@@ -64,20 +74,34 @@ export default class GalleryComponent extends Component {
                 original: process.env.PUBLIC_URL + '/images/13.jpg',
                 thumbnail: process.env.PUBLIC_URL + '/images/13_thumb.png'
             },
-            // {
-            //     original: process.env.PUBLIC_URL + '/images/14.jpg',
-            //     thumbnail: process.env.PUBLIC_URL + '/images/14_thumb.png'
-            // },
-            // {
-            //     original: process.env.PUBLIC_URL + '/images/15.jpg',
-            //     thumbnail: process.env.PUBLIC_URL + '/images/15_thumb.png'
-            // }
+            {
+                original: process.env.PUBLIC_URL + '/images/14.jpg',
+                thumbnail: process.env.PUBLIC_URL + '/images/14_thumb.png'
+            },
+            {
+                original: process.env.PUBLIC_URL + '/images/15.jpg',
+                thumbnail: process.env.PUBLIC_URL + '/images/15_thumb.png'
+            }
           ];
 
         return (
             <div className = "container">
                 <div className = "row">
-                    <ImageGallery items={images} />
+                    <div className = "col-md-12">
+                            <br/>
+                        <div class="alert alert-dismissible alert-primary">
+                            <h4><FontAwesomeIcon icon = {faImage}/> Gallery</h4>
+                            <small>Sorry for tooking all this pictures but I hope you like it! </small>
+                        </div>
+                    </div>
+
+                    <ImageGallery 
+                        items={images}  
+                        lazyLoad={false}
+                        infinite={this.state.infinite}
+                        showBullets={this.state.showBullets}
+                        thumbnailPosition={this.state.thumbnailPosition}
+                    />
                 </div>
             </div>
         )
